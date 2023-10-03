cfdcd453bf29cf771ecd179f875827e919b0b926
SteDor <136347112+SteDor@users.noreply.github.com>
2023-10-03 08:09:39 +0200

    Add files via upload
    Update 03.10.2023 8:00

---
+++ MultipleChoiceQuestion.php
@@ -0,0 +1,6 @@
+<?php 
+class MultipleChoiceQuestion extends Question {
+    private $NumberOfRIghtAnswers;
+    
+}
+?>
--- simpletest1.php
+++ simpletest1.php
@@ -1,6 +1,8 @@
 <?php 
-include ('C:\xampp\htdocs\02.10.2023_Php\Question');
-$question1 = new Question(['dasjk', 5 , 'nb'], 'Mathe', 'Wie lautet die Frage', 'XxXxXxXxXx', false);
+
+include ('.\..\02.10.2023_Php\MathQuestion');
+
+$question1 = new MathQuestion(['dasjk', 5 , 'nb'], 'Mathe', 'Wie lautet die Frage', 'XxXxXxXxXx', false);
 
 echo ( $question1->getQuestionCategory() . ', '. $question1->getQuestionText() . ', ' . $question1->getDiscriptionQuestion(). ', '. $question1->getCheckedAnswerUser() . '. ')
 
