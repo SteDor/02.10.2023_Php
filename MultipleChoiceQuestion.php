79b325b95bd8cde5aeaab71d12075581077ba0ac
SteDor <136347112+SteDor@users.noreply.github.com>
2023-10-03 11:17:27 +0200

    Add files via upload
    update 03.10.2023 11:17

--- MultipleChoiceQuestion.php
+++ MultipleChoiceQuestion.php
@@ -1,6 +1,62 @@
 <?php 
 class MultipleChoiceQuestion extends Question {
-    private $NumberOfRIghtAnswers;
+
+    public $NumberOfRightAnswers;
+    public $Answers  = array('protected', 'private', 'public', 'abstract');
+    public $RightAnswers = array(true, true,true, false);
+
+    function __construct($Answers, $QuestionCategory, $QuestionText, $DiscriptionQuestion, $RightAnswers) {
+        $this->Answers = $Answers;
+        $this->QuestionCategory = $QuestionCategory;
+        $this->QuestionText = $QuestionText;
+        $this->DiscriptionQuestion = $DiscriptionQuestion;
+        $this->RightAnswers =$RightAnswers;
+        echo 'MultiQuestion created<br>';
+
+        }
+
+    public function CountRightAnswers($RightAnswers) {
+        $NumberOfRightAnswers = 0; 
+        for ($i=0; $i<sizeof($RightAnswers);$i++){
+            if($RightAnswers[$i] == true){
+                $NumberOfRightAnswers++;
+            }
+        }
+        echo ('Count= '. $NumberOfRightAnswers);
+        return $NumberOfRightAnswers;
+    }
+
+
+
+    function GetQuestion(){}
+     
+    function CheckAnswer(){}
     
+    function GetAnswer(){}
+
+    function get_NumberOfRightAnswers($NumberOfRightAnswers) {
+        return $this->NumberOfRightAnswers;
+      }
+    
+    public function set_NumberOfRightAnswers($NumberOfRIghtAnswers): self {
+		$this->NumberOfRightAnswers = $NumberOfRIghtAnswers;
+		return $this;
+	}
+    function get_AnswersArray($Answers ) {
+        return $this->Answers ;
+      }
+    
+    public function set_AnswersArray ($Answers ): self {
+		$this->Answers  = $Answers ;
+		return $this;
+	}
+    function get_RightAnswers($RightAnswers ) {
+        return $this->RightAnswers;
+      }
+    
+    public function set_RightAnswers ($RightAnswers): self {
+		$this->RightAnswers  = $RightAnswers ;
+		return $this;
+	}
 }
 ?>
--- simpletest1.php
+++ simpletest1.php
@@ -1,9 +1,38 @@
 <?php 
 
 include ('.\..\02.10.2023_Php\MathQuestion');
+include ('.\..\02.10.2023_Php\MultipleChoiceQuestion.php');
 
-$question1 = new MathQuestion(['dasjk', 5 , 'nb'], 'Mathe', 'Wie lautet die Frage', 'XxXxXxXxXx', false);
+$MultiQuestion = new MultipleChoiceQuestion(['protected', 'private', 'public', 'abstract'], 'Coding', 'Welche dieser Begriffe sind "access modifiers" in PHP? ','Choose the right answers: ', [true, true,true, false]);
+//for ($i=0;$i<sizeof($MultiQuestion->Answers);$i++){
+//    echo ($MultiQuestion->Answers[$i] . ', <br>');
+//};
+//'<br>';
+//$MultiQuestion->CountRightAnswers($MultiQuestion->RightAnswers);
+//echo '<br>';
+//echo (var_dump($MultiQuestion->RightAnswers[0]) . '<br>');
+echo ('<h3>'.$MultiQuestion->get_QuestionText(). '</h3>');
+echo ($MultiQuestion->getDiscriptionQuestion(). '<br><br>');
+?>
 
-echo ( $question1->getQuestionCategory() . ', '. $question1->getQuestionText() . ', ' . $question1->getDiscriptionQuestion(). ', '. $question1->getCheckedAnswerUser() . '. ')
+<input type="checkbox" id="answer1" name="answer1" value="<? var_dump($MultiQuestion->RightAnswers[0])?>"> 
+<label for="answer1"> <?= $MultiQuestion->Answers[0]?></label><br>
+<input type="checkbox" id="answer2" name="answer2" value="<? var_dump($MultiQuestion->RightAnswers[1])?>">
+<label for="answer2"> <?= $MultiQuestion->Answers[1]?></label><br>
+<input type="checkbox" id="answer3" name="answer3" value="<? var_dump($MultiQuestion->RightAnswers[2])?>">
+<label for="answer3"> <?= $MultiQuestion->Answers[2]?></label><br> 
+<input type="checkbox" id="answer4" name="answer4" value="<? var_dump($MultiQuestion->RightAnswers[3])?>">
+<label for="answer4"> <?= $MultiQuestion->Answers[3]?></label><br>
+<br>
+<button type="submit" >Submit Answer</button>
+
+<?php 
+// echo ($MultiQuestion::Get_NumberOfRightAnswers() . '<br>');
+// echo ( $MultiQuestion->getQuestionCategory() . ', '. $MultiQuestion->getQuestionText() . ', ' . $MultiQuestion->getDiscriptionQuestion(). ', '. $MultiQuestion->getCheckedAnswerUser() . '. ');
+
+
+//$question1 = new MathQuestion(['dasjk', 5 , 'nb'], 'Mathe', 'Wie lautet die Frage', 'XxXxXxXxXx', false);
+//
+//echo ( $question1->getQuestionCategory() . ', '. $question1->getQuestionText() . ', ' . $question1->getDiscriptionQuestion(). ', '. $question1->getCheckedAnswerUser() . '. ')
 
 ?>
